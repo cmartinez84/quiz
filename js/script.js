ec77e5d654ba190556c38b6b11d8fcc4a4aacc47
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-08-12 11:09:24 -0700

    finish HTML and apply basic boostrap stylings. no custom CSS or javascript yet.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var scorePhp = 0;
+var scoreRuby = 0
+var scoreJava = 0;
+var scoreCss = 0;
+var scoreCsharp = 0;
+
+
+$(function(){
+  $("#submitButton").click(function(){
+    var answer1 = $("#question1").text();
+    var answer2 = $("#question2").text();
+    var answer3 = $("#question3").text();
+    var answer4 = $("#question4").text();
+    var answer5 = $("#question5").text();
+    alert(answer1, answer5, answer4, answer2, answer3);
+
+  });
+
+
+
+});
